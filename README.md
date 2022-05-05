76ef40ca61bab69b80b8a44a2eed50d77107cd2c
Su Kang <sukang2017@u.northwestern.edu>
2022-05-05 11:26:00 -0500

    Adds Create Event submission handler

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 SpringBoot demo pracatic
 
 starter - initial commit with static events lis
-form - Adds Create Event form display t
+form - Adds Create Event form display
+form2 - Adds Create Event submission handler
--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,14 +13,15 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+    private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
+//        List<String> events = new ArrayList<>();
+//        events.add("Code With Pride");
+//        events.add("Strange Loop");
+//        events.add("Apple WWDC");
+//        events.add("SpringOne Platform");
         model.addAttribute("events",events);
         return "events/index";
     }
@@ -28,5 +31,15 @@ public class EventController {
     public String renderCreateEventForm(){
         return "events/create";
     }
+
+    // lives at /event/create
+    // It is ok to have two handlers with same path b/c they handle two
+    // different requests
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:";
+    }
+
 
 }
