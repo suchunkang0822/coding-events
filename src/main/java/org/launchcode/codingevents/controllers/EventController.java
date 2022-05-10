dca62c666474c4faa5fd2ba6eb5da5fdada16f73
Su Kang <sukang2017@u.northwestern.edu>
2022-05-10 00:05:39 -0500

    Added description property to Event

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,10 +3,7 @@ package org.launchcode.codingevents.controllers;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,8 +34,8 @@ public class EventController {
     // It is ok to have two handlers with same path b/c they handle two
     // different requests
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName){
-        events.add(new Event(eventName));
+    public String createEvent(@RequestParam String eventName,@RequestParam String eventDescription){
+        events.add(new Event(eventName,eventDescription));
         return "redirect:";
     }
 
