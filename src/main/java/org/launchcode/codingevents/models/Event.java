d256dd101bb9b42c00fbfa8300cabfed997741ac
Su Kang <sukang2017@u.northwestern.edu>
2022-05-16 21:21:48 -0500

    Adds validation annotations to model

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,16 +1,28 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
+    @NotBlank
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
     private String name;
+
+    @Size(max = 500, message = "Description is too long.")
     private String description;
+
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
+
     private int id;
     private static int nextId=1;
 
-    public Event(String name, String description) {
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id = nextId;
         nextId++;
     }
@@ -29,6 +41,14 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public int getId() {
